85b7e98b4e39b94c9b3d65df054a27ce895be2b2
Mathieu Garstecki <mathieu.garstecki@gmail.com>
2019-02-14 11:17:04 +0100

    Comment on how the chars list of the password generator was chosen.

--- internal/utils/password.go
+++ internal/utils/password.go
@@ -8,6 +8,8 @@ import (
 
 func GenerateRandomPassword(length int) string {
 	rand.Seed(time.Now().UnixNano())
+
+	// Removed usual ambiguous chars : one/lowercase_l, zero/letter_O
 	chars := []rune("ABCDEFGHIJKLMNPQRSTUVWXYZ" +
 		"abcdefghijkmnpqrstuvwxyz" +
 		"23456789" +
@@ -17,7 +19,7 @@ func GenerateRandomPassword(length int) string {
 	for i := 0; i < length; i++ {
 		b.WriteRune(chars[rand.Intn(len(chars))])
 	}
-	str := b.String() // E.g. "ExcbsVQs"
+	str := b.String()
 
 	return str
 }
