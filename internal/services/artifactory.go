f1f9c9d6462062c7dd19834be419e8fe4733b634
Mathieu Garstecki <mathieu.garstecki@gmail.com>
2019-02-19 15:46:17 +0100

    Handle duplication of tenant name in project name (remove it in project name if present).

--- internal/services/artifactory.go
+++ internal/services/artifactory.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/atlassian/go-artifactory/pkg/artifactory"
 	"github.com/ca-gip/artifactory-operator/internal/config"
@@ -60,9 +61,12 @@ func permissionName(permission string, fields *ArtifactoryInformation) string {
 }
 
 func artifactoryRepositoryKey(fields *ArtifactoryInformation, stage string) string {
+	tenantPrefix := fields.Tenant + "-"
+	projectNameWithoutTenant := strings.TrimPrefix(fields.ProjectName, tenantPrefix)
+
 	return fmt.Sprintf("%s-%s-docker-%s-%s",
 		fields.Tenant,
-		fields.ProjectName,
+		projectNameWithoutTenant,
 		stage,
 		fields.Location)
 }
