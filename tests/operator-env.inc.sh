f33edf5336f5047481436f61a8b3c28c72507685
Aurelien Gabet <aurelien.gabet@gmail.com>
2019-10-14 18:04:59 +0200

    switch to vault kv2 and new path structure

--- tests/operator-env.inc.sh
+++ tests/operator-env.inc.sh
@@ -3,17 +3,16 @@
 
 #ARTIFACTORY
 export ARTI_OP_CLUSTER_LOCATION=intranet
-export ARTI_OP_ARTIFACTORY_SERVER_URL=http://rct-registry.saas.cagip.gca/artifactory
+export ARTI_OP_ARTIFACTORY_SERVER_URL=https://rct-registry.saas.cagip.gca
 export ARTI_OP_ARTIFACTORY_SERVER_USER=admin_operator
-export ARTI_OP_ARTIFACTORY_SERVER_PASSWORD=Admin@123
-
+export ARTI_OP_ARTIFACTORY_SERVER_PASSWORD=
 #XRAY
 export ARTI_OP_XRAY_SERVER_URL=http://rct-xray.saas.cagip.gca/artifactory
 export ARTI_OP_XRAY_BINMGRID=arti-rct
 export ARTI_OP_XRAY_SERVER_USER=admin
-export ARTI_OP_XRAY_SERVER_PASSWORD=password
+export ARTI_OP_XRAY_SERVER_PASSWORD=
 
 #VAULT
 export ARTI_OP_VAULT_SERVER_URL=https://rct-vault.saas.cagip.gca
-export ARTI_OP_VAULT_TOKEN=s.vM297rbqrcNlJq6B1J1eZOEC
+export ARTI_OP_VAULT_TOKEN=
 export ARTI_OP_CLUSTER_DNSSUBDOMAIN=internet-hp
