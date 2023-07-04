8cbc2519f54746c761b10f648962f048f1a6117a
Kévin PEREZ <kevin.perez@ca-gip.fr>
2023-07-04 09:43:18 +0200

    🐛 fix error log message

--- internal/utils/artifactory.go
+++ internal/utils/artifactory.go
@@ -1,14 +1,15 @@
 package utils
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func HasANon404Error(err error, resp *http.Response) bool {
-	if err != nil {
-		if resp == nil || resp.StatusCode != http.StatusNotFound {
-			return true
-		}
+	if err != nil && (resp == nil || resp.StatusCode != http.StatusNotFound) {
+		fmt.Println("Technical error occured when looking at existing group: ", err)
+		return true
 	}
+
 	return false
 }
