75a8c5535f37694e7446b294234d6e9d70a03294
Mehdi HAYANI <mehdi_hayani@ext.carrefour.com>
2025-06-17 13:07:55 +0200

    is v2

--- internal/utils/validationtools.go
+++ internal/utils/validationtools.go
@@ -38,15 +38,27 @@ func CheckMandatoryParametersV2(project *v1.Project) error {
 	return nil
 }
 
-// IsV2Project determines if a Project resource is v2 based on its API version
+// IsV2Project determines if a Project resource is v2 based on its API version or other criteria
 func IsV2Project(project *v1.Project) bool {
 	// Always log at Info level to ensure visibility
 	Log.Info().Msgf("IsV2Project: Project %s has APIVersion: '%s'", project.Name, project.APIVersion)
 
-	// Check for v2 in multiple ways to be more robust
-	isV2 := strings.Contains(project.APIVersion, "v2") ||
-		strings.Contains(project.APIVersion, "V2") ||
-		project.APIVersion == "cagip.github.com/v2"
+	// First check API version if it's set
+	isV2 := false
+	if project.APIVersion != "" {
+		isV2 = strings.Contains(project.APIVersion, "v2") ||
+			strings.Contains(project.APIVersion, "V2") ||
+			project.APIVersion == "cagip.github.com/v2"
+	}
+
+	// If API version is empty or not v2, check other criteria
+	if !isV2 {
+		// Check if the project was created with a v2 manifest (based on missing stages)
+		if len(project.Spec.Stages) == 0 {
+			Log.Info().Msgf("IsV2Project: Project %s identified as v2 based on missing stages", project.Name)
+			isV2 = true
+		}
+	}
 
 	Log.Info().Msgf("IsV2Project: Project %s is v2: %v", project.Name, isV2)
 
